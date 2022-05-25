d05eb5ec47bdc5e656a73844ee43daca5f1ab55f
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-24 19:36:12 -0500

    4)ExplorerService class with static methods

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService")
+
 const explorers = Reader.readJsonFile("explorers.json"); 
-console.log(explorers)
+//console.log(explorers)
 // Return a list of explorers from explorers.json
+
+console.log(ExplorerService.filterByMission(explorers,"node"))
+console.log("Explorers in node: " + ExplorerService.getAmountOfExplorersByMission(explorers,"node"))
+console.log(ExplorerService.getExplorersUsernameByMission(explorers,"node"))

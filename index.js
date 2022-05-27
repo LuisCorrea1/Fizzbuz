9fcc48cd6de55b606401376cbbdba4925982543e
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-27 18:04:04 -0500

    8) Code fixes from linter

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Reader = require("./lib/utils/Reader");
-const ExplorerService = require("./lib/services/ExplorerService")
+const ExplorerService = require("./lib/services/ExplorerService");
 
 const explorers = Reader.readJsonFile("explorers.json"); 
 //console.log(explorers)
 // Return a list of explorers from explorers.json
 
-console.log(ExplorerService.filterByMission(explorers,"node"))
-console.log("Explorers in node: " + ExplorerService.getAmountOfExplorersByMission(explorers,"node"))
-console.log(ExplorerService.getExplorersUsernameByMission(explorers,"node"))
+console.log(ExplorerService.filterByMission(explorers,"node"));
+console.log("Explorers in node: " + ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
+console.log(ExplorerService.getExplorersUsernameByMission(explorers,"node"));
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,9 +1,9 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Test for ExplorerService", () => {
     test("Requirement 1: Get all explorers in node", () => {
-        const explorers = [{mission: "node"}]
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
-    })
-})
+    });
+});

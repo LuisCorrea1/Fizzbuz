261db05b014cb5bb7e6e59f3752ac3d26443e323
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-26 19:21:04 -0500

    5) Jest & unit test for ExplorerService class

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,9 @@
+const ExplorerService = require("./../../lib/services/ExplorerService")
+
+describe("Test for ExplorerService", () => {
+    test("Requirement 1: Get all explorers in node", () => {
+        const explorers = [{mission: "node"}]
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
+        expect(explorersInNode.length).toBe(1);
+})
+})

b019bc28e36cc58a39815d89c01ea9d486041082
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-30 19:05:47 -0500

    Update in index file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
 
 const explorers = Reader.readJsonFile("explorers.json"); 
 //console.log(explorers)
 // Return a list of explorers from explorers.json
 
-console.log(ExplorerService.filterByMission(explorers,"node"));
-console.log("Explorers in node: " + ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
-console.log(ExplorerService.getExplorersUsernameByMission(explorers,"node"));
+//console.log(ExplorerService.filterByMission(explorers,"node"));
+//console.log("Explorers in node: " + ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
+//console.log(ExplorerService.getExplorersUsernameByMission(explorers,"node"));
+
+const explorersWithFizzBuzzTrick= explorers.map((exp) => FizzbuzzService.applyValidationInExplorer(exp))
+console.log(explorersWithFizzBuzzTrick)
